6934137c04839aaa8e6c0c4886390feadef3dfd6
hojiii <shimn3060@naver.com>
2023-04-28 22:07:38 +0900

    Fix: 모달 중앙위치 고정으로 수정

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,57 +5,52 @@ import {
   MoveModalSubbtn,
   MoveModalTitle,
   MoveModalbtn,
+  ModalWrapper,
 } from '../shared/SpaceStyles';
 
 import { useNavigate } from 'react-router-dom';
 
-
 function Modal({ setIsModal, modalTitle, onButtonClick, redirectPath }) {
+  const navi = useNavigate();
 
-    const navi = useNavigate();
+  const closeModal = () => {
+    setIsModal(false);
+  };
 
-    const closeModal = () => {
-        setIsModal(false);
-      };
-
-      const handleButtonClick  = () => {
-        onButtonClick();
-        navi(redirectPath);
-        closeModal();
-    };
+  const handleButtonClick = () => {
+    onButtonClick();
+    navi(redirectPath);
+    closeModal();
+  };
 
   return (
-    <ModalFullBackground
-    width= '2135px'
-    height= '1045px'
-    left= '-10px'
-    >
-      <MoveModal>
-        <MoveModalTitle>{modalTitle}</MoveModalTitle>
-        
-        <MoveModalbtn
-          onClick={handleButtonClick}
-          
-          width='75px'
-          height= '33px'
-          left='16px'
-          top='121px'
-          padding='4px 10px'
-        >
-          네
-        </MoveModalbtn>
-        <MoveModalSubbtn
-          onClick={closeModal}
+    <ModalFullBackground>
+      <ModalWrapper>
+        <MoveModal>
+          <MoveModalTitle>{modalTitle}</MoveModalTitle>
 
-          width='75px'
-          height= '33px'
-          left='97px'
-          top='121px'
-          padding='4px 10px'
-        >
-          아니요
-        </MoveModalSubbtn>
-      </MoveModal>
+          <MoveModalbtn
+            onClick={handleButtonClick}
+            width="75px"
+            height="33px"
+            left="16px"
+            top="121px"
+            padding="4px 10px"
+          >
+            네
+          </MoveModalbtn>
+          <MoveModalSubbtn
+            onClick={closeModal}
+            width="75px"
+            height="33px"
+            left="97px"
+            top="121px"
+            padding="4px 10px"
+          >
+            아니요
+          </MoveModalSubbtn>
+        </MoveModal>
+      </ModalWrapper>
     </ModalFullBackground>
   );
 }
