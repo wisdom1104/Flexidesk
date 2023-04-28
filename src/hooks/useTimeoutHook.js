11064e6e560a83700b67a2a64810827ce9fbc558
eunseonJeong <suni980215@gmail.com>
2023-04-28 17:09:31 +0900

    오타수정

--- src/hooks/useTimeoutHook.js
+++ src/hooks/useTimeoutHook.js
@@ -1,37 +1,34 @@
-// import React, { useState, useEffect } from 'react';
-// import { useDispatch  } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { cookies } from '../shared/cookies';
+import React, { useEffect, useState } from 'react'
+import { cookies } from '../shared/cookies';
+import { useNavigate } from 'react-router-dom';
 
-// const useTimeout = (action, args = []) => {
-//   const dispatch = useDispatch();
-//   const navi = useNavigate();
-//   const token = cookies.get("token")
-//   const role = cookies.get("role")
-//   const [showSkeleton, setShowSkeleton] = useState(true);
+function useTimeoutHook(dispatchFunc) {
+    const [showSkeleton, setShowSkeleton] = useState(true);
 
-//   useEffect(() => {
-//     if (!token) {
-//       navi('/');
-//     } else if (role !== 'ADMIN') {
-//       navi('/');
-//     } else {
-//       const loadData = async () => {
-//         try {
-//           dispatch(action(...args));
-//           setShowSkeleton(false);
-//         } catch (error) {
-//           console.error(error);
-//         }
-//       };
-//       const timer = setTimeout(() => {
-//         loadData();
-//       }, 3000);
-//       return () => clearTimeout(timer);
-//     }
-//   }, [token, role]);
+    const token = cookies.get('token');
+    const navi = useNavigate();
+    
+    useEffect(() => {
+        if (!token) {
+          navi('/');
+        } else {
+          const loadData = async () => {
+            try {
+            dispatchFunc();
+            } catch (error) {
+              console.log(error);
+            }
+          };
+    
+          const timer = setTimeout(() => {
+            loadData();
+            setShowSkeleton(false);
+          }, 2000);
+          return () => clearTimeout(timer);
+        }
+      }, []);
 
-//   return showSkeleton;
-// };
+  return {}
+}
 
-// export default useTimeout;
+export default useTimeoutHook
