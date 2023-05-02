4126dbe7a9b1829ae6089f84b44b2ec4b8b7dbe7
정은선 <jeong-eunseon@jeong-eunseon-ui-MacBookAir.local>
2023-05-03 04:32:03 +0900

    fix: useAuthFormValidation에 useCallback 삭제

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,21 +1,23 @@
+import { css } from 'styled-components';
+
 export const theme = {
-    //color
-    blue: '#07133B',
-    lightblue: '#0B2475',
-    darkblue: '#314563',
+  //color
+  blue: '#07133B',
+  lightblue: '#0B2475',
+  darkblue: '#314563',
 
-    darkmint: '#4A8E8B',
-    mint: '#65BAB6',
-    lightmint: '#B6DFDD',
-    // backfroundMint: '#F4FBF9',
-    backgroundMint: '#DEF1EF',
+  darkmint: '#4A8E8B',
+  mint: '#65BAB6',
+  lightmint: '#B6DFDD',
+  // backfroundMint: '#F4FBF9',
+  backgroundMint: '#DEF1EF',
 
-    black: '#15161A',
-    darkgrey: '#8B93A6',
-    grey: '#C9CDD6',
-    lightgrey: '#F1F2F4',
+  black: '#15161A',
+  darkgrey: '#8B93A6',
+  grey: '#C9CDD6',
+  lightgrey: '#F1F2F4',
 
-    error: '#FF5353',
+  error: '#FF5353',
 
-    white : '#ffffff',
-}
+  white: '#ffffff',
+};
