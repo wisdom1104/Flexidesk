81ac326727d74c4cd7d4075e642d876506aad93e
정은선 <jeong-eunseon@jeong-eunseon-ui-MacBookAir.local>
2023-05-05 19:06:30 +0900

    fix: 스타일 수정

--- src/pages/user/UserStyled.jsx
+++ src/pages/user/UserStyled.jsx
@@ -26,14 +26,17 @@ export const StLoginContain = styled.div`
 
 export const StLoginForm = styled.form`
   width: 420px;
+  height: 100%;
+
   background: var(--white);
   box-shadow: 0px 8px 20px rgba(175, 200, 197, 0.25);
+  
   border-radius: 8px;
   padding: 40px;
   display: flex;
   flex-direction: column;
   flex: 1;
-  height: 100%;
+
   align-items: center;
   margin-top: 100px;
 `;
